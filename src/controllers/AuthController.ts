9a2fb4592ade424c3c03f849826f53c3e6837453
peduhvitor <pedrovitor4012@gmail.com>
2023-02-06 01:56:45 -0300

    Criando os métodos nas actions

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+
+export const signin = async (req: Request, res: Response) => {
+
+}
+
+export const signup = async (req: Request, res: Response) => {
+
+}
---
+++ src/controllers/UserController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+export const getStates = async (req: Request, res: Response) => {
+
+}
+
+export const info = async (req: Request, res: Response) => {
+
+}
+
+export const editAction = async (req: Request, res: Response) => {
+
+}
