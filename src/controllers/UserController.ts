dccf3456a9eec9dee851f1d81aa3650bf6bb6936
peduhvitor <pedrovitor4012@gmail.com>
2023-02-13 12:09:32 -0300

    Pegando informaçÕes do usuário

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 import State from "../models/State";
+import User from "../models/User";
+import Category from "../models/Category";
+import Ad from "../models/Ad";
 
 export const getStates = async (req: Request, res: Response) => {
     const states = await State.find();
@@ -7,7 +10,29 @@ export const getStates = async (req: Request, res: Response) => {
 }
 
 export const info = async (req: Request, res: Response) => {
+    const { token } = req.query
 
+    const user = await User.findOne({ token })
+    const state = await State.findById(user?.state)
+    const ads = await Ad.find({idUser: user?._id.toString()})
+
+
+    // Puxar anúncios do usuário
+
+    let adList:any = []
+
+    ads.forEach(async(item )=> {
+        const cat = await Category.findById(item.category)
+        adList.push({...item, category: cat?.slug})
+    })
+
+    res.json({
+        name: user?.name,
+        email: user?.email,
+        state: state?.name,
+        ads: adList
+    })
+    
 }
 
 export const editAction = async (req: Request, res: Response) => {
